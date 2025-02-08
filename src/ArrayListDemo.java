7535308eb9639530c610f844b521238e49e74bec
caglatopak <160266658+caglatopak@users.noreply.github.com>
2025-02-08 23:19:24 +0100

    firstcommit

---
+++ src/ArrayListDemo.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+
+
+        ArrayList<String> isimlerListesi = new ArrayList<>();
+
+
+        // add listeye ekleme yapma
+
+        isimlerListesi.add("ozan");
+        isimlerListesi.add("sevinc");
+
+        System.out.println(isimlerListesi);
+
+        isimlerListesi.add("ceyda");
+        isimlerListesi.add("salih");
+
+        System.out.println(isimlerListesi);
+
+
+        // get ile index numarasina gore bulma
+
+        System.out.println(isimlerListesi.get(2) + isimlerListesi.get(3) + isimlerListesi.getLast() + isimlerListesi.getFirst());
+
+        // listedeki bir referansi guncellemek icin set kullanilir
+
+        isimlerListesi.set(2, " ayse ");
+
+        System.out.println(isimlerListesi.get(2));
+
+
+        // listeden referans cikarmak icin remove kullanilir. eski degeri bulmak istersek return kullaniriz.
+        String silinenIsim = isimlerListesi.remove(1);
+        System.out.println(silinenIsim + "listeden cikarilmistir");
+
+        isimlerListesi.add("filiz");
+        isimlerListesi.add("aynur");
+        isimlerListesi.add("eda");
+        isimlerListesi.add("leyla");
+        isimlerListesi.add("melahat");
+
+        System.out.println(isimlerListesi);
+
+        // size fonksiyonu ile listenin boyutu iogrenilir;
+
+        isimlerListesi.size();
+
+        System.out.println("listemizin element sayisi = " + isimlerListesi.size());
+
+        // listede isim arama contains ile bulurnur
+
+        System.out.println("listemizde ayse ismi " + isimlerListesi.contains(" ayse "));
+        System.out.println("listemizde fatma ismi " + isimlerListesi.contains("fatma"));
+        System.out.println("listemizde leyla ismi " + isimlerListesi.contains("leyla"));
+        System.out.println("listemizde fadime ismi " + isimlerListesi.contains("fadime"));
+
+        System.out.println(isimlerListesi);
+    }
+}
